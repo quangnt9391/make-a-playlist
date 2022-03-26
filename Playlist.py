3c9b81e071d3cf9b09188470cf7fc9838ace644d
Nguyen Quang <102234167+quangnt9391@users.noreply.github.com>
2022-03-26 15:05:39 +0700

    update work with list of videos

--- Playlist.py
+++ Playlist.py
@@ -10,29 +10,67 @@ def read_video():
 	video = Video(title, link)
 	return video
 
+def read_videos():
+	videos = []
+	total = input("how many videos: ")
+	total = int(total)
+	for i in range(total):
+		print("Enter video:", i+1)
+		video = read_video()
+		videos.append(video)
+	return videos
+
 def print_video(video):
 	print("--------------")
 	print("video title:" ,video.title, end="")
 	print("video link: ", video.link, end="")
 
-def write_video_to_txt(video):
-	with open("data.txt", "w") as file:
-		file.write(video.title)
-		file.write(video.link)
+def print_videos(videos):
+	print("\n************************\n")
+	total = len(videos)
+	print("Total video: ", str(total) + "\n")
+	for i in range(total):
+		print("video ", i+1)
+		print_video(videos[i])
 
-def read_video_from_txt():
-	with open("data.txt", "r") as file:
-		title = file.readline()
-		link = file.readline()
-		video = Video(title, link)
+
+def write_video_to_txt(video, file):
+	# with open("data.txt", "w") as file:
+	file.write(video.title)
+	file.write(video.link)
+
+def read_video_from_txt(file):
+	# with open("data.txt", "r") as file:
+	title = file.readline()
+	link = file.readline()
+	video = Video(title, link)
 	return video
+
+def write_videos_to_txt(videos):
+	total = len(videos)
+	with open("data.txt", "w") as file:
+		file.write(str(total) + "\n")
+		for i in range(total):
+			write_video_to_txt(videos[i], file)
+	print("file write successfully!")
+
+def read_videos_from_txt():
+	with open("data.txt", "r") as file:
+		videos = []
+		total = file.readline()
+		total = int(total)
+		for i in range(total):
+			video = read_video_from_txt(file)
+			videos.append(video)
+	return videos
+
 
 
 def main():
-	video = read_video()
-	write_video_to_txt(video)
-	video = read_video_from_txt()
-	print_video(video)
+	videos = read_videos()
+	write_videos_to_txt(videos)
+	videos = read_videos_from_txt()
+	print_videos(videos)
 
 
 
