63afb22fcfca7d8562994df762949c60f8ebcd72
Nguyen Quang <102234167+quangnt9391@users.noreply.github.com>
2022-03-26 10:32:35 +0700

    update read video from file

--- Playlist.py
+++ Playlist.py
@@ -11,18 +11,29 @@ def read_video():
 	return video
 
 def print_video(video):
-	print("video title:" ,video.title)
-	print("video link: ", video.link)
+	print("--------------")
+	print("video title:" ,video.title, end="")
+	print("video link: ", video.link, end="")
 
 def write_video_to_txt(video):
 	with open("data.txt", "w") as file:
 		file.write(video.title)
 		file.write(video.link)
 
+def read_video_from_txt():
+	with open("data.txt", "r") as file:
+		title = file.readline()
+		link = file.readline()
+		video = Video(title, link)
+	return video
+
+
 def main():
 	video = read_video()
-	print_video(video)
 	write_video_to_txt(video)
+	video = read_video_from_txt()
+	print_video(video)
+
 
 
 main()
