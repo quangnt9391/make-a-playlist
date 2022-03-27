539c12cee8e0c3e82f3bb5da9de5f581704b6b18
Nguyen Quang <102234167+quangnt9391@users.noreply.github.com>
2022-03-27 09:02:08 +0700

    update create a playlist and main menu

--- Playlist.py
+++ Playlist.py
@@ -4,6 +4,13 @@ class Video:
 		self.title = title
 		self.link = link
 
+class Playlist:
+	def __init__(self, name, description, rating, videos):
+		self.name = name
+		self.description = description
+		self.rating = rating
+		self.videos = videos
+
 def read_video():
 	title = input("Enter video title: ") + "\n"
 	link = input("Enter video link: ") + "\n"
@@ -19,6 +26,14 @@ def read_videos():
 		video = read_video()
 		videos.append(video)
 	return videos
+	
+def read_playlist():
+	name = input("Enter Playlist name: ") + "\n"
+	description = input("Enter playlist description: ") + "\n"
+	rating = input("Enter playlist rating: ") + "\n"
+	videos = read_videos()
+	playlist = Playlist(name, description, rating, videos)
+	return playlist
 
 def print_video(video):
 	print("--------------")
@@ -33,6 +48,11 @@ def print_videos(videos):
 		print("video ", i+1)
 		print_video(videos[i])
 
+def print_playlist(playlist):
+	print("Playlist's name: ", playlist.name, end="")
+	print("Playlist's description: ", playlist.description, end="")
+	print("Playlist's rating: ", playlist.rating, end="")
+	print_videos(playlist.videos)
 
 def write_video_to_txt(video, file):
 	# with open("data.txt", "w") as file:
@@ -46,31 +66,56 @@ def read_video_from_txt(file):
 	video = Video(title, link)
 	return video
 
-def write_videos_to_txt(videos):
+def write_videos_to_txt(videos, file):
 	total = len(videos)
-	with open("data.txt", "w") as file:
-		file.write(str(total) + "\n")
-		for i in range(total):
-			write_video_to_txt(videos[i], file)
-	print("file write successfully!")
+	# with open("data.txt", "w") as file:
+	file.write(str(total) + "\n")
+	for i in range(total):
+		write_video_to_txt(videos[i], file)
 
-def read_videos_from_txt():
-	with open("data.txt", "r") as file:
-		videos = []
-		total = file.readline()
-		total = int(total)
-		for i in range(total):
-			video = read_video_from_txt(file)
-			videos.append(video)
+def read_videos_from_txt(file):
+	# with open("data.txt", "r") as file:
+	videos = []
+	total = file.readline()
+	total = int(total)
+	for i in range(total):
+		video = read_video_from_txt(file)
+		videos.append(video)
 	return videos
 
+def write_playlist_to_txt(playlist):
+	with open("data.txt", "w") as file:
+		file.write(playlist.name)
+		file.write(playlist.description)
+		file.write(playlist.rating)
+		write_videos_to_txt(playlist.videos, file)
+	print("write playlist successfully!!")
 
+def read_playlist_from_txt():
+	with open("data.txt", "r") as file:
+		name = file.readline()
+		description = file.readline()
+		rating = file.readline()
+		videos = read_videos_from_txt(file)
+	playlist = Playlist(name, description, rating, videos)
+	return playlist
+def main_menu():
+	print(" --------------------- ")
+	print("| 1. Creat playlist   |")
+	print("| 2. Show playlist    |")
+	print("| 3. Play a video     |")
+	print("| 4. Add a video      |")
+	print("| 5. Update playlist  |")
+	print("| 6. Delete a video   |")
+	print("| 7. Save and Exit    |")
+	print(" --------------------- ")
 
 def main():
-	videos = read_videos()
-	write_videos_to_txt(videos)
-	videos = read_videos_from_txt()
-	print_videos(videos)
+	main_menu()
+	playlist = read_playlist()
+	write_playlist_to_txt(playlist)
+	playlist = read_playlist_from_txt()
+	print_playlist(playlist)
 
 
 
